41362fa7b802b9e652098607df20b5e2b255c1d3
LazaroHurtado <hurtadolazaro12@gmail.com>
2020-11-15 14:10:36 -0500

    fixed cron job

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
+const mongo = require("../controller/mongo");
+const { Task } = require("../models/tasks");
 const controller = require("../controller/task");
 
+router.get("/get", async (req, res) => {
+  const { statusCode, response } = await mongo.find(Task, {});
+  res.status(statusCode).send({ response });
+});
 router.post("/create", async (req, res) => {
   const data = req.body;
 
